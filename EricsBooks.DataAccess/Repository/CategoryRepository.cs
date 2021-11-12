284445c21dd450b457bfd685208adc6c5b813fbb
eric <90633675+Ericbatiste1@users.noreply.github.com>
2021-11-11 21:15:29 -0500

    Added ICategoryRepository.cs and CategoryRepository.cs

---
+++ EricsBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using EricsBooks.DataAccess.Repository.IRepository;
+using EricsBooks.Models;
+using EricsBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EricsBooks.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+
+        }
+    }
+}
