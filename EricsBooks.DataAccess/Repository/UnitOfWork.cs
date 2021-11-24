1ab864be54f53c07512b0591ebb7ac17254d79bc
eric <90633675+Ericbatiste1@users.noreply.github.com>
2021-11-23 22:39:37 -0500

    fixed typo

--- EricsBooks.DataAccess/Repository/UnitOfWork.cs
+++ EricsBooks.DataAccess/Repository/UnitOfWork.cs
@@ -22,7 +22,7 @@ namespace EricsBooks.DataAccess.Repository
 
         public ICategoryRepository Category { get; private set; }
         public ISP_Call SP_Call { get; private set; }
-        public ICoverTypeRepository Cover { get; private set; }
+        public ICoverTypeRepository CoverType { get; private set; }
 
         public void Dispose()
         {
